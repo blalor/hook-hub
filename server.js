44286ed73f19544aad9cf89f334882054bee5fbc
Brian Lalor <blalor@bravo5.org>
2013-07-04 08:14:30 -0400

    Track number of open file descriptors
    After adding the sandbox support, I'm getting not-infrequent EMFILE
    errors, which appears to be due to too many open file descriptors.
    This logging is to try to track that down.
    
    domain.js:66
        throw er;
              ^
    Error: accept EMFILE
        at errnoException (net.js:769:11)
        at TCP.onconnection (net.js:1017:24)

--- server.js
+++ server.js
@@ -1,11 +1,30 @@
 "use strict";
 
+var os     = require("os");
+var fs     = require("fs");
 var bunyan = require("bunyan");
+
 var HookServer = require("./lib/hook-server").HookServer;
 
 var log = bunyan.createLogger({
     name: "hub",
     level: "debug"
 });
+
+// if we're on linux, periodically count the number of open files
+if (os.platform() === "linux") {
+    var intervalId = setInterval(function() {
+        fs.readdir("/proc/" + process.pid + "/fd/", function(err, entries) {
+            if (err) {
+                log.error(err, "unable to get fd count");
+            } else {
+                log.info(entries.length + " open file descriptors");
+            }
+        });
+    }, 10000);
+    
+    // don't let the process keep running for our sake
+    // intervalId.unref(); // not in node 0.8.12
+}
 
 new HookServer(7000, log);
