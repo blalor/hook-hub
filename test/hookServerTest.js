60fb51af8eb242830bf048e96125b491ab28fdb7
Brian Lalor <blalor@bravo5.org>
2013-10-01 20:51:22 -0400

    Make paths without extra bits still work
    Fixes #3, which was caused by #2. :-)

---
+++ test/hookServerTest.js
@@ -0,0 +1,99 @@
+"use strict";
+
+var bunyan = require("bunyan");
+var portfinder = require("portfinder");
+
+var expect  = require("chai").expect;
+var Q       = require("q");
+var request = require("request");
+
+var HookServer = require("../lib/hook-server").HookServer;
+
+describe("hook server", function() {
+    var log = bunyan.createLogger({
+        name: "hook-server",
+        level: "error"
+    });
+
+    var server;
+    var endpoint;
+    var mockEndpointDao;
+    var mockSubscriptionDao;
+    
+    beforeEach(function(done) {
+        mockEndpointDao = {};
+        mockSubscriptionDao = {};
+        
+        portfinder.getPort(function (err, port) {
+            if (err) {
+                done(err);
+            } else {
+                server = new HookServer(port, mockEndpointDao, mockSubscriptionDao, log);
+                endpoint = "http://localhost:" + port;
+                
+                done();
+            }
+        });
+    });
+    
+    afterEach(function() {
+        mockSubscriptionDao = null;
+        mockEndpointDao = null;
+        endpoint = null;
+        server = null;
+    });
+    
+    it("accepts published POST data with extra path info", function(done) {
+        mockEndpointDao.get = function() {
+            // return a mock endpoint
+            return Q({
+                type: "whatever",
+                tags: []
+            });
+        };
+        
+        mockSubscriptionDao.getByTypeAndTags = function() {
+            // return an empty list of subscriptions
+            return Q([]);
+        };
+        
+        request.post({
+            url: endpoint + "/publish/some-id/baz/bap",
+            json: {
+                foo: "bar"
+            }
+        }, function(err, resp, body) {
+            expect(err).to.be.null;
+            expect(resp.statusCode).to.equal(204);
+            
+            done();
+        });
+    });
+    
+    it("accepts published POST data without extra path info", function(done) {
+        mockEndpointDao.get = function() {
+            // return a mock endpoint
+            return Q({
+                type: "whatever",
+                tags: []
+            });
+        };
+        
+        mockSubscriptionDao.getByTypeAndTags = function() {
+            // return an empty list of subscriptions
+            return Q([]);
+        };
+
+        request.post({
+            url: endpoint + "/publish/some-id",
+            json: {
+                foo: "bar"
+            }
+        }, function(err, resp) {
+            expect(err).to.be.null;
+            expect(resp.statusCode).to.equal(204);
+            
+            done();
+        });
+    });
+});
