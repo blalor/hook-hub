334358b8ca6715330e4cdd2fb3116c60ce6f69e7
Brian Lalor <blalor@bravo5.org>
2013-10-01 20:13:36 -0400

    Inject DAOs into HookServer
    Will ease/enable testing.

--- server.js
+++ server.js
@@ -1,11 +1,31 @@
 "use strict";
 
-var bunyan = require("bunyan");
-var HookServer = require("./lib/hook-server").HookServer;
+var bunyan     = require("bunyan");
+var redis      = require("redis");
+var Q          = require("q");
+
+var HookServer   = require("./lib/hook-server").HookServer;
+var endpoint     = require("./lib/daos/endpoint");
+var subscription = require("./lib/daos/subscription");
 
 var log = bunyan.createLogger({
     name: "hub",
     level: "debug"
 });
 
-new HookServer(7000, log);
+
+// don't start listening until the database is ready for connections
+var dbConnPromise = Q.defer();
+
+var db = redis.createClient();
+db.on("ready", dbConnPromise.resolve);
+db.on("error", dbConnPromise.reject);
+
+dbConnPromise.promise
+    .done(function() {
+        var endpointDao = new endpoint.EndpointDAO(db, log.child({endpointDao: true}));
+        var subscriptionDao = new subscription.SubscriptionDAO(db, log.child({subscriptionDao: true}));
+        
+        new HookServer(7000, endpointDao, subscriptionDao, log);
+    });
+
